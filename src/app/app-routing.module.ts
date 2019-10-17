9fad434ac25814febe95b44e030632fd87af991d
Unknown <robinelzeard@gmail.com>
2019-10-17 14:39:57 +0200

    mise en place d'un routage identique

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { PageComponent } from './page/page.component';
 const routes: Routes = [  
   {path:'', component: HomeComponent},
   {path:'home', component: HomeComponent},
-  {path:'page', component: PageComponent}
+  {path:'pages/:pageId', component: PageComponent}
 ];
 
 @NgModule({
--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router'
 
 @Component({
   selector: 'app-page',
   templateUrl: './page.component.html',
   styleUrls: ['./page.component.scss']
 })
-export class PageComponent implements OnInit {
 
-  constructor() { }
+export class PageComponent implements OnInit {
+  id
+  constructor(
+    private Route:ActivatedRoute
+  ) { 
+    this.id = this.Route.snapshot.paramMap.get('pageId');
+  }
 
   ngOnInit() {
   }
