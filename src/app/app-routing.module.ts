2ae4257894b05f2795f6275d0383899ebc4377fd
Unknown <robinelzeard@gmail.com>
2019-10-17 13:09:43 +0200

    menu copié

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PageComponent } from './page/page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [  
+  {path:'', component: HomeComponent},
+  {path:'home', component: HomeComponent},
+  {path:'page', component: PageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
